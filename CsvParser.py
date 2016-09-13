7eda25e7707bc10d30efd0d47b3564232f8c01a5
Ahmed Mehfooz <ahmed.mehfooz@gmail.com>
2016-09-13 22:04:48 +0100

    Made small changes to Schema

--- CsvParser.py
+++ CsvParser.py
@@ -3,6 +3,16 @@ import sys
 import getpass
 import MySQLdb
 import datetime
+import argparse
+
+# Turn Off Warnings for MySQL
+from warnings import filterwarnings, resetwarnings
+
+filterwarnings('ignore', category = MySQLdb.Warning)
+
+
+# Uncomment to Turn On Warnings
+# resetwarnings()
 
 ################################################################################################################
 #                                         Database Info                                                        #
@@ -12,9 +22,12 @@ SQL_USER = 'root'
 SQL_DB = 'RF_Data'
 ################################################################################################################
 class CsvParser:
-	def __init__(self, filename):
+	def __init__(self, filename, passwd):
+		self.pwd = ""
+		self.fname = filename
 		self.file = open(filename)
 		self.reader = csv.reader(self.file)
+		self.comment = ""
 		# Get last test ID
 		self.current_test = 0
 
@@ -22,7 +35,7 @@ class CsvParser:
 		self.current_file = 0
 
 		# Set up DB Connection
-		SQLPWD = getpass.getpass("Database Password: ")
+		SQLPWD = passwd
 		self.db = MySQLdb.connect(host=SQL_HOST, user=SQL_USER, passwd=SQLPWD, db=SQL_DB)
 
 		self.curs = self.db.cursor()
@@ -45,29 +58,31 @@ class CsvParser:
 				continue
 			if row[0] == "Date:":
 				self.date = datetime.datetime.strptime(row[1], '%m/%d/%Y %I:%M %p')
+				# Change to SQL Date Format
 				self.date = self.date.strftime('%Y-%m-%d %H:%M')
 				
 				date = row[1].split("/")
 				date[2] = date[2][2:]
 				date = "".join(date)
-				self.current_file = date.replace(" ", "").replace(":", "")
-				
-				self.current_file = self.current_file[:len(self.current_file)-2]
+				self.current_file = int(self.fname.split("_")[-1].split(".")[0][:-2].replace("-",""))
+
 			if " Comment:" in row:
 				self.comment = row[3]
 			if row[0] == "SN":
 				self.fieldnames = row
 				for i in range(len(self.fieldnames)):
-					self.fieldnames[i] = self.fieldnames[i].strip()
+					self.fieldnames[i] = self.fieldnames[i].strip().upper()
 					self.fieldindex[self.fieldnames[i]] = i
 				break
 
 	# Read through rows and enter into database as appropriate
 	def insertToSQL(self):
-		print self.fieldnames
 		file_entered = 0
 		prev_chain = -1	
 		for row in self.reader:
+			for i in range(len(row)):
+				row[i] = row[i].strip()
+
 			if file_entered == 0:
 				self.insertFile(row)
 				file_entered = 1
@@ -76,29 +91,36 @@ class CsvParser:
 				self.current_test += 1
 				self.insertTestRun(row)
 
+				if "BLOCKER_FREQ(MHZ)" in self.fieldnames:
+					self.insertRXBlocker(row)
+
 			if "PER" in self.fieldnames:
 				self.insertRXPER(row)
 
-			if "Region" in self.fieldnames:
+			if "REGION" in self.fieldnames:
 				self.insertSpectrum(row)
 
-			if "Target_Power" in self.fieldnames and "LP_EVM" in self.fieldnames:
+			if "TARGET_POWER" in self.fieldnames and "LP_EVM" in self.fieldnames:
 				self.insertTXEVM(row)
 
 			if "TXEVM" in self.fieldnames:
 				self.insertTXEVMPower(row)
 
 			prev_chain = row[self.fieldindex["CHAIN"]]
+
 	# Insert File entry into File table in SQL Database
 	def insertFile(self, row):
-		query = """INSERT into File (FileID, SN, Revision, TestType, COMMENT, TESTRUNTIME)
-			VALUES ({0}, {1}, {2}, {3}, {4}, {5})
-			""".format(self.current_file, repr(row[self.fieldindex["SN"]]), repr(row[self.fieldindex["REVISION"]]), \
-				repr(row[self.fieldindex["TestType"]].strip()), repr(self.comment), repr(self.date))
+		query = """INSERT into File (FileID, SN, Station, Revision, TestType, COMMENT, TESTRUNTIME)
+			VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6})
+			""".format(self.current_file, repr(row[self.fieldindex["SN"]]), repr(row[self.fieldindex["STATION"]]), repr(row[self.fieldindex["REVISION"]]), \
+				repr(row[self.fieldindex["TESTTYPE"]].strip()), repr(self.comment), repr(self.date))
 
-		print query
-		self.curs.execute(query)
-		self.db.commit()
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
+			exit()
 
 	# Insert Test Run entry into TestRun table in SQL Database
 	def insertTestRun(self, row):
@@ -109,70 +131,103 @@ class CsvParser:
 
 		chan1 = chans[0]
 
+
 		query = """INSERT into TestRun (TestRunID, FileID, CHAIN, BW, CHAN, CHAN_2, BAND, RATE, ANT) 
 			VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})
 			""".format(self.current_test, self.current_file, row[self.fieldindex["CHAIN"]], \
 				repr(row[self.fieldindex["BW"]]), repr(chan1), repr(chan2), \
-				repr(row[self.fieldindex["Band"]]), \
+				repr(row[self.fieldindex["BAND"]]), \
 				repr(row[self.fieldindex["RATE"]]), repr(row[self.fieldindex["ANT"]]))
 
-		self.curs.execute(query)
-		self.db.commit()
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
+			exit()
 
 	# Insert RXPER entry
 	def insertRXPER(self, row):
-		query = """INSERT into RXPER (TestRunID, PWR, PER)
-			VALUES ({0}, {1}, {2})
-			""".format(self.current_test, row[self.fieldindex["PWR"]], row[self.fieldindex["PER"]])
+		query = """INSERT into RXPER (TestRunID, FileID, PWR, PER)
+			VALUES ({0}, {1}, {2}, {3})
+			""".format(self.current_test, self.current_file, row[self.fieldindex["PWR"]], row[self.fieldindex["PER"]])
 
-		self.curs.execute(query)
-		self.db.commit()
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
 
 	# Insert TXEVM entry
 	def insertTXEVM(self, row):
-		query = """INSERT into TXEVM (TestRunID, Target_Power, LP_EVM, LP_Power,
-			LP_freqErrorHz, LP_ClockErrorPpm) VALUES ({0}, {1}, {2}, {3}, {4}, {5})
-			""".format(self.current_test, row[self.fieldindex["Target_Power"]], \
-			row[self.fieldindex["LP_EVM"]], row[self.fieldindex["LP_Power"]], \
-			row[self.fieldindex["LP_freqErrorHz"]], row[self.fieldindex["LP_ClockErrorPpm"]])
+		query = """INSERT into TXEVM (TestRunID, FileID, Target_Power, LP_EVM, LP_Power,
+			LP_freqErrorHz, LP_ClockErrorPpm) VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6})
+			""".format(self.current_test, self.current_file, row[self.fieldindex["TARGET_POWER"]], \
+			row[self.fieldindex["LP_EVM"]], row[self.fieldindex["LP_POWER"]], \
+			row[self.fieldindex["LP_FREQERRORHZ"]], row[self.fieldindex["LP_CLOCKERRORPPM"]])
 
-		self.curs.execute(query)
-		self.db.commit()
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
 
 	# Insert TXEVMPower entry
 	def insertTXEVMPower(self, row):
 		query = """INSERT into TXEVMPower (TestRunID, FileID, EVM Floor, TXEVM)
-			VALUES ({0}, {1}, {2})
-			""".format(self.current_test, self.fileID, row[self.fieldindex["EVM Floor"]], row[self.fieldindex["TXEVM"]])
+			VALUES ({0}, {1}, {2}, {3})
+			""".format(self.current_test, self.current_file, row[self.fieldindex["EVM FLOOR"]], row[self.fieldindex["TXEVM"]])
 
-		self.curs.execute(query)
-		self.db.commit()
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
+
+	# Insert RXBlocker entry
+	def insertRXBlocker(self, row):
+		query = """INSERT into RXBlocker (TestRunID, FileID, BlockerFreq, BlockerPower, LVL_PWR_Offset)
+			VALUES ({0}, {1}, {2}, {3}, {4})
+			""".format(self.current_test, self.current_file, row[self.fieldindex["BLOCKER_FREQ(MHZ)"]], \
+				row[self.fieldindex["BLOCKER_POWER(DBM)"]], row[self.fieldindex["BLOCKER_LVLPWROFFSET(DB)"]])
+
+		try:
+			self.curs.execute(query)
+			self.db.commit()
+		except Exception as e:
+			print e ,"@", query
 
 	# Insert Spectrum entry
 	def insertSpectrum(self, row):
 		passed = 0
-		if "PASS" in row[self.fieldindex["Pass/Fail"]]:
+		if "PASS" in row[self.fieldindex["PASS/FAIL"]]:
 			passed = 1
 
-		query = """INSERT into Spectrum (TestRunID, Region, LP_FreqOffset, Mask_Power, Mask_Limit,
-			Pass, MARGIN, Fail) VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})
-			""".format(self.current_test, repr(row[self.fieldindex["Region"]]), \
-				row[self.fieldindex["LP_FreqOffset"]], row[self.fieldindex["Mask_Power"]], \
-				row[self.fieldindex["Mask_Limit"]], passed, \
-				row[self.fieldindex["Margin"]], row[self.fieldindex["Fail(%)"]])
+		query = """INSERT into Spectrum (TestRunID, FileId, Region, LP_FreqOffset, Mask_Power, Mask_Limit,
+			Pass, MARGIN, Fail) VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})
+			""".format(self.current_test, self.current_file, repr(row[self.fieldindex["REGION"]]), \
+				row[self.fieldindex["LP_FREQOFFSET"]], row[self.fieldindex["MASK_POWER"]], \
+				row[self.fieldindex["MASK_LIMIT"]], passed, \
+				row[self.fieldindex["MARGIN"]], row[self.fieldindex["FAIL(%)"]])
 
 		try:
 			self.curs.execute(query)
+			self.db.commit()
 		except Exception as e:
-			print query
- 			print e
- 			exit()
-		self.db.commit()
+			print e ,"@", query
 
 	# Insert Spectrum Plot entry
 	def insertSpectrumPlot(self, row):
 		pass
-		
-with CsvParser(sys.argv[1]) as parser:
+
+# Setup Argument Parser
+
+parser = argparse.ArgumentParser()
+parser.add_argument('CSV', type=str, nargs='+', help='Enter CSV File Name')
+parser.add_argument('-p', type=str, help='Password for SQL Database')
+
+args = parser.parse_args()
+
+with CsvParser(" ".join(args.CSV), args.p) as parser:
 	parser.readFileHeader()
 	parser.insertToSQL()
--- create_database.sql
+++ create_database.sql
@@ -8,20 +8,10 @@ CREATE Database IF NOT EXISTS RF_Data;
 use RF_Data
 
 --
--- Table structure for table `StationID`
---
-CREATE TABLE IF NOT EXISTS `Station` (
-	`StationID` int(16) PRIMARY KEY,
-	`Bench` varchar(32),
-	`Equipment` varchar(32),
-	`Calibaration` varchar(256)
-) ENGINE=InnoDB;
-
---
 -- Table structure for table `SWConfig`
 --
 CREATE TABLE IF NOT EXISTS `SWConfig` (
-	`SWConfigID` int(16) PRIMARY KEY,
+	`SWConfigID` int(16) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`ImageName` varchar(256),
 	`BuildNumber` int(16)
 ) ENGINE=InnoDB;
@@ -31,31 +21,30 @@ CREATE TABLE IF NOT EXISTS `SWConfig` (
 --
 
 CREATE TABLE IF NOT EXISTS `File` (
-	`FileID` int(32) PRIMARY KEY,
-	`SN` varchar(16),
+	`FileID` BIGINT PRIMARY KEY,
+	`SN` varchar(32),
+	`Station` varchar(32),
 	`Revision` varchar(32),
-	`StationID` int(16), 
-	`COMMENT` varchar(256),
-	`SWConfigID` int(16),
-	`TESTRUNTIME` DATETIME,
-	
-	FOREIGN KEY (`StationID`) REFERENCES `Station`(`StationID`),
-	FOREIGN KEY (`SWConfigID`) REFERENCES `SWConfig`(`SWConfigID`)
-)
+	`TestType` enum('Mask', 'EVM', 'Sens', 'PwrAccuracy', 'Simu', 'Blocker'),
+	`COMMENT` varchar(256) DEFAULT NULL,
+	`SWConfigID` int(16) DEFAULT NULL,
+	`TESTRUNTIME` DATETIME
+) ENGINE=InnoDB;
 -- 
 -- Table structure for table `TestRun`
 --
 CREATE TABLE IF NOT EXISTS `TestRun` (
-	`TestRunID` int(32) PRIMARY KEY,
-	`FileID` int(32),
-	`TestType` enum('Mask', 'EVM', 'Sens'),
+	`TestRunID` int(32),
+	`FileID` BIGINT, 
 	`CHAIN` int(8),
 	`BW` enum('20', '40', '80', '160'),
 	`CHAN` int(8),
+	`CHAN_2` int(8),
 	`BAND` enum('2G', '5G'),
-	`RATE` varchar(10),
-	`ANT` int(8),
+	`RATE` enum('v0-1', 'v9-1', 'm7', 'v8-1', 'v7-1', 'm0', '54', '1l', '11s', '11'),
+	`ANT` enum('0', '1', '2', '3', '0-2', '1-3'),
 
+	PRIMARY KEY (`TestRunID`, `FileID`),
 	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 -- 
@@ -63,68 +52,76 @@ CREATE TABLE IF NOT EXISTS `TestRun` (
 --
 CREATE TABLE IF NOT EXISTS `RXPER` (
 	`TestRunID` int(32),
+	`FileID` BIGINT,
 	`PWR` int(8),
 	`PER` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `TXEVM` (
 	`TestRunID` int(32),
-	`TargetPower` decimal,
+	`FileID` BIGINT,
+	`Target_Power` decimal,
 	`LP_EVM` decimal,
 	`LP_Power` decimal,
 	`LP_freqErrorHz` decimal,
 	`LP_ClockErrorPpm` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `RXSensitivity` (
 	`TestRunID` int(32),
+	`FileID` BIGINT,
 	`PER Floor` decimal,
 	`Low End Sensitivity` decimal,
 	`High End Sensitivity` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `TXEVMPower` (
 	`TestRunID` int(32),
+	`FileID` BIGINT,
 	`EVM Floor` decimal,
 	`TXEVM` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`) 
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `TXPower` (
 	`TestRunID` int(32),
+	`FileID` BIGINT,
 	`TargetPower` decimal,
 	`MeasurementMethod` varchar(32),
 	`MeasuredPower` decimal,
 	`MeasuredError` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `RXBlocker` (
 	`TestRunID` int(32),
+	`FileID` BIGINT,
 	`BlockerFreq` decimal,
 	`BlockerPower` decimal,
+	`LVL_PWR_Offset` decimal,
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `SpectrumMask` (
+CREATE TABLE IF NOT EXISTS `Spectrum` (
 	`TestRunID` int(32),
-	`Region` varchar(32),
+	`FileID` BIGINT,
+	`Region` enum('LO_A', 'LO_B', 'LO_C', 'LO_D', 'LO_E', 'UP_A', 'UP_B', 'UP_C', 'UP_D', 'UP_E'),
 	`LP_FreqOffset` decimal,
 	`Mask_Power` decimal,
 	`Mask_Limit` decimal,
-	`Pass/Fail` boolean,
-	`MARGIN` decimal,
-	`FAIL(%)` decimal,
+	`Pass` boolean,
+	`Margin` decimal,
+	`Fail` decimal,
 	SpectrumPlotID int(32),
 
-	FOREIGN KEY (`TestRunID`) REFERENCES `TestRun`(`TestRunID`)
+	FOREIGN KEY (`FileID`) REFERENCES `File`(`FileID`)
 ) ENGINE=InnoDB;
